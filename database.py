7effa067dce1cd017b4e5fcb2f75948e5ae6cf93
Jay <jayliu@zymber.tech>
2023-03-10 13:44:24 +0800

    fix create_engine錯誤  File "C:\Project-Python\Fastapi_SQLAlchemy\database.py", line 21, in <module>     engine = create_engine(              ^^^^^^^^^^^^^^   File "<string>", line 2, in create_engine   File "C:\Project-Python\Fastapi_SQLAlchemy\.venv\Lib\site-packages\sqlalchemy\util\deprecations.py", line 277, in warned     return fn(*args, **kwargs)  # type: ignore[no-any-return]            ^^^^^^^^^^^^^^^^^^^   File "C:\Project-Python\Fastapi_SQLAlchemy\.venv\Lib\site-packages\sqlalchemy\engine\create.py", line 693, in create_engine     raise TypeError( TypeError: Invalid argument(s) 'encoding' sent to create_engine(), using configuration MySQLDialect_pymysql/QueuePool/Engine.  Please check that the keyword arguments are appropriate for this combination of components.

--- database.py
+++ database.py
@@ -19,7 +19,7 @@ SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:qazqsedc/@127.0.0.1:3306/db"
 
 # echo=True表示引擎將用repr()函式記錄所有語句及其引數列表到日誌
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, encoding='utf8', echo=True
+    SQLALCHEMY_DATABASE_URL, echo=True
 )
 
 # SQLAlchemy中，CRUD是通過會話進行管理的，所以需要先建立會話，
